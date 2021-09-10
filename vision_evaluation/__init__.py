264b19e9e59e9621ca8584c1c94428dd64be68dd
xingpeng@microsoft.com <xingpeng@microsoft.com>
2021-09-10 23:51:44 +0000

    add unit test

--- vision_evaluation/__init__.py
+++ vision_evaluation/__init__.py
@@ -1,6 +1,6 @@
 from .evaluators import MeanAveragePrecisionEvaluatorForMultipleIOUs, MeanAveragePrecisionEvaluatorForSingleIOU, TopKAccuracyEvaluator, ThresholdAccuracyEvaluator, PrecisionEvaluator, \
-    RecallEvaluator, AveragePrecisionEvaluator, EceLossEvaluator, F1ScoreEvaluator, Evaluator, MemorizingEverythingEvaluator, EvaluatorAggregator
+    RecallEvaluator, AveragePrecisionEvaluator, EceLossEvaluator, F1ScoreEvaluator, Evaluator, MemorizingEverythingEvaluator, EvaluatorAggregator, AveragePrecisionNPointsEvaluator
 
 __all__ = ['MeanAveragePrecisionEvaluatorForMultipleIOUs', 'MeanAveragePrecisionEvaluatorForSingleIOU', 'TopKAccuracyEvaluator', 'ThresholdAccuracyEvaluator', 'PrecisionEvaluator', 'RecallEvaluator',
            "AveragePrecisionEvaluator", "EceLossEvaluator", 'F1ScoreEvaluator', 'Evaluator', 'MemorizingEverythingEvaluator', 'EvaluatorAggregator', 'TagWiseAccuracyEvaluator',
-           'TagWiseAveragePrecisionEvaluator']
+           'TagWiseAveragePrecisionEvaluator', 'AveragePrecisionNPointsEvaluator']
