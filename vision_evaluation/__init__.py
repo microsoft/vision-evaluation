32cd926e0d199c3b00f1bf338e1f15b368d965af
Ping Jin <pinjin@microsoft.com>
2021-10-12 21:22:32 -0700

    fix init

--- vision_evaluation/__init__.py
+++ vision_evaluation/__init__.py
@@ -1,6 +1,7 @@
 from .evaluators import MeanAveragePrecisionEvaluatorForMultipleIOUs, MeanAveragePrecisionEvaluatorForSingleIOU, TopKAccuracyEvaluator, ThresholdAccuracyEvaluator, PrecisionEvaluator, \
-    RecallEvaluator, AveragePrecisionEvaluator, EceLossEvaluator, F1ScoreEvaluator, Evaluator, MemorizingEverythingEvaluator, EvaluatorAggregator
+    RecallEvaluator, AveragePrecisionEvaluator, EceLossEvaluator, F1ScoreEvaluator, Evaluator, MemorizingEverythingEvaluator, EvaluatorAggregator, TagWiseAveragePrecisionEvaluator, \
+    TagWiseAccuracyEvaluator, MeanAveragePrecisionNPointsEvaluator, BalancedAccuracyScoreEvaluator
 
 __all__ = ['MeanAveragePrecisionEvaluatorForMultipleIOUs', 'MeanAveragePrecisionEvaluatorForSingleIOU', 'TopKAccuracyEvaluator', 'ThresholdAccuracyEvaluator', 'PrecisionEvaluator', 'RecallEvaluator',
            "AveragePrecisionEvaluator", "EceLossEvaluator", 'F1ScoreEvaluator', 'Evaluator', 'MemorizingEverythingEvaluator', 'EvaluatorAggregator', 'TagWiseAccuracyEvaluator',
-           'TagWiseAveragePrecisionEvaluator', 'AveragePrecisionNPointsEvaluator']
+           'TagWiseAveragePrecisionEvaluator', 'MeanAveragePrecisionNPointsEvaluator', 'BalancedAccuracyScoreEvaluator']
