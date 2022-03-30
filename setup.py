668066556394d92570e55a26d349adb2350a05bc
msxingpeng <xingpeng@microsoft.com>
2022-03-30 03:50:16 +0000

    fixing comments

--- setup.py
+++ setup.py
@@ -34,5 +34,5 @@ setuptools.setup(name='vision-evaluation',
                      'pycocotools',
                      'pycocoevalcap',
                      'opencv-python',
-                     'Pillow~=9.0.1'
+                     'Pillow>=6.2.2'
                  ])
